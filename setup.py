ea851f44c58b18e87bd0e80fafa49cdc1d06ad28
SeanNobel <sensho19950503@gmail.com>
2023-12-12 11:43:17 +0900

    update module name bm -> brainmagick

--- setup.py
+++ setup.py
@@ -11,21 +11,20 @@ from pathlib import Path
 
 from setuptools import setup
 
-NAME = 'bm'
-DESCRIPTION = (
-    'Framework for training deep neural network on MEG and EEG data.')
+NAME = "brainmagick"
+DESCRIPTION = "Framework for training deep neural network on MEG and EEG data."
 
-URL = 'https://github.com/facebookresearch/brainmagick'
-EMAIL = 'defossez@meta.com'
-AUTHOR = 'Alexandre Défossez'
-REQUIRES_PYTHON = '>=3.7.0'
+URL = "https://github.com/facebookresearch/brainmagick"
+EMAIL = "defossez@meta.com"
+AUTHOR = "Alexandre Défossez"
+REQUIRES_PYTHON = ">=3.7.0"
 
-for line in open('bm/__init__.py'):
+for line in open("bm/__init__.py"):
     line = line.strip()
-    if '__version__' in line:
+    if "__version__" in line:
         context = {}
         exec(line, context)
-        VERSION = context['__version__']
+        VERSION = context["__version__"]
 
 
 HERE = Path(__file__).parent
@@ -34,8 +33,8 @@ HERE = Path(__file__).parent
 REQUIRED = [i.strip() for i in open("requirements.txt") if "://" not in i]
 
 try:
-    with open(HERE / "README.md", encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with open(HERE / "README.md", encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -44,18 +43,18 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=['bm'],
+    packages=["bm"],
     install_requires=REQUIRED,
     include_package_data=True,
-    license='Creative Commons Attribution-NonCommercial 4.0 International',
+    license="Creative Commons Attribution-NonCommercial 4.0 International",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
